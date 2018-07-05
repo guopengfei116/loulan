d74287279d2cc80808105c9a3411b75ce48e6c71
PengFei <626138959@qq.com>
2018-07-05 21:46:36 +0800

    实现"商家申请与信息完善"相关接口

--- loulan-shop-web/src/main/java/com/loulan/shop/controller/SellerController.java
+++ loulan-shop-web/src/main/java/com/loulan/shop/controller/SellerController.java
@@ -1,12 +1,11 @@
 package com.loulan.shop.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.loulan.pojo.TbSeller;
 import com.loulan.sellergoods.service.SellerService;
 import com.loulan.vo.HttpResult;
-import jdk.nashorn.internal.ir.annotations.Reference;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.loulan.vo.PageResult;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SellerController {
@@ -14,13 +13,65 @@ public class SellerController {
     @Reference
     private SellerService sellerService;
 
-    // 更新
-    @PostMapping("/update")
-    public HttpResult update(@RequestBody TbSeller seller) {
+    /**
+     * 主键查询
+     *
+     * @param  id  主键
+     * @return     实体对象
+     * */
+    @GetMapping("/findOne")
+    public TbSeller findOne(String id) {
+        return sellerService.findOne(id);
+    }
+
+    /**
+     * 分页sql条件查询
+     *
+     * @param  page   页码
+     * @param  size   页大小
+     * @param  t      实体对象，封装了查询条件
+     * @return        分页实体对象
+     * */
+    @PostMapping("/findPageByWhere")
+    public PageResult findPageByWhere(@RequestParam(defaultValue = "1") Integer page,
+                                      @RequestParam(defaultValue = "10") Integer size,
+                                      @RequestBody(required = false) TbSeller t) {
+        return sellerService.findPageByWhere(page, size, t);
+    }
+
+    /**
+     * 添加，商家默认为未审核状态，0：未审核 1：已审核 2：审核未通过 3：关闭
+     *
+     * @param t 实体对象
+     */
+    @PostMapping("/add")
+    public HttpResult add(@RequestBody TbSeller t) {
         HttpResult httpResult;
 
         try {
-            sellerService.update(seller);
+            t.setStatus("0");
+            sellerService.add(t);
+            httpResult = HttpResult.ok("添加成功");
+        }catch (Exception e) {
+            e.printStackTrace();
+            httpResult = HttpResult.fail("添加失败");
+        }
+
+        return httpResult;
+    }
+
+    /**
+     * 修改
+     *
+     * @param t 实体对象
+     */
+    @PostMapping("/update")
+    public HttpResult update(@RequestBody TbSeller t) {
+        HttpResult httpResult;
+
+        // 通过更新方法修改状态
+        try {
+            sellerService.update(t);
             httpResult = HttpResult.ok("修改成功");
         }catch (Exception e) {
             e.printStackTrace();
@@ -30,19 +81,4 @@ public class SellerController {
         return httpResult;
     }
 
-    // 添加
-    @PostMapping("/add")
-    public HttpResult add(@RequestBody TbSeller seller) {
-        HttpResult httpResult;
-
-        try {
-            sellerService.add(seller);
-            httpResult = HttpResult.ok("添加成功");
-        }catch (Exception e) {
-            e.printStackTrace();
-            httpResult = HttpResult.fail("添加失败");
-        }
-
-        return httpResult;
-    }
 }
