1fbfc6814ef6aa1073ebe14a60300fe47a739ee0
PengFei <626138959@qq.com>
2018-07-06 11:52:32 +0800

    商家后台实现商品模板信息获取接口

---
+++ loulan-shop-web/src/main/java/com/loulan/shop/controller/TypeTemplateController.java
@@ -0,0 +1,31 @@
+package com.loulan.shop.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.loulan.pojo.TbTypeTemplate;
+import com.loulan.sellergoods.service.TypeTemplateService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/typeTemplate")
+public class TypeTemplateController {
+    
+    @Reference
+    private TypeTemplateService typeTemplateService;
+
+    /**
+     * 主键查询
+     *
+     * @param  id  主键
+     * @return     实体对象
+     * */
+    @GetMapping("/findOne")
+    public TbTypeTemplate findOne(Long id) {
+        return typeTemplateService.findOne(id);
+    }
+
+}
