d3e79834817706a2154bddc11536b08aafba65dd
PengFei <626138959@qq.com>
2018-07-06 11:49:45 +0800

    商家后台提供两个分类列表父找子接口，一个是全部数据，一个用于select

--- loulan-shop-web/src/main/java/com/loulan/shop/controller/ItemCatController.java
+++ loulan-shop-web/src/main/java/com/loulan/shop/controller/ItemCatController.java
@@ -1,6 +1,7 @@
 package com.loulan.shop.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.loulan.pojo.TbItemCat;
 import com.loulan.sellergoods.service.ItemCatService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,20 @@ public class ItemCatController {
     private ItemCatService itemCatService;
 
     /**
-     * 通过父类获取子类下拉列表
+     * 获取子类列表
+     *
+     * @param  parentId  父类id
+     * @return           实体集合
+     */
+    @GetMapping("/findByParentId")
+    public List<TbItemCat> findByParentId(Long parentId) {
+        TbItemCat itemCat = new TbItemCat();
+        itemCat.setParentId(parentId);
+        return itemCatService.findByWhere(itemCat);
+    }
+
+    /**
+     * 获取子类下拉列表
      *
      * @param  parentId  父类id
      * @return           实体id与name(as text)构成的集合：[ {id, text}, {id, text}, ... ]
